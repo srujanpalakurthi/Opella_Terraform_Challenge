549276815da15ac15ca56ff03c86ca186609f59c
Srujan Kumar Palakurthi <srujan.palakurthi@gmail.com>
2025-09-22 22:24:44 +0530

    Test environemnt confiuration

--- test/main.tf
+++ test/main.tf
@@ -4,23 +4,23 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "dev-rg"
+  name     = "test-rg"
   location = "Central India"
 }
 module "vnet" {
   source              = "../modules/vnet"
-  vnet_name           = "dev-vnet"
+  vnet_name           = "test-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  subnet_name         =  "dev-subnet"
+  subnet_name         =  "test-subnet"
   subnet_address_space = ["10.0.2.0/24"]
   
 }
 
 module "vm" {
   source              = "../modules/vm"
-  vm_name             = "dev-vm"
+  vm_name             = "test-vm"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   subnet_id           = module.vnet.subnet_id
